c09b019472509abeb64f5369b9faf0c4812440e6
Aymo19 <m.hettes3@gmail.com>
2024-11-04 12:42:15 +0100

    I forgot these in the second commit, mb

--- src/Frontend/TestGUI.java
+++ src/Frontend/TestGUI.java
@@ -9,8 +9,8 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-
 public class TestGUI extends Application {
+	
     @Override
     public void start(Stage primaryStage) {
         try {
@@ -22,7 +22,7 @@ public class TestGUI extends Application {
             Pane root = new Pane();
             Scene scene = new Scene(root,400,400);
             
-            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            
             primaryStage.setScene(scene);
             primaryStage.show();
             
