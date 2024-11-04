c2468afc2117e91be8a4e8ee5cb6e5024564710e
Aymo19 <m.hettes3@gmail.com>
2024-11-04 20:10:17 +0100

    Backend planning and Admin methods creation

--- src/Backend/Groups/Admin.java
+++ src/Backend/Groups/Admin.java
@@ -1,5 +1,7 @@
 package Backend.Groups;
 
+import java.util.ArrayList;
+
 public final class Admin extends User{
 	
 	public Admin(int ID, String User_name) {
@@ -7,11 +9,27 @@ public final class Admin extends User{
 		
 	}
 	
-	public void addUsers() {
+	public void addUsers(ArrayList<User> list,User newUser) {
+		for(User n : list) {
+			if(n.getUserID() == newUser.getUserID()) {
+				System.out.println("User already exists!");
+				return;
+			}
+		}
 		
+		list.add(newUser);
+		System.out.println("User " + newUser.getUserName() + " with ID: " + newUser.getUserID() + 
+				" has been created and added to the database.");
 	}
 	
-	public void removeUser() {
-		
+	public void removeUser(ArrayList<User> list,User delUser) {
+		for(User n : list) {
+			if(n.getUserID() == delUser.getUserID()) {
+				list.remove(delUser);
+				System.out.println("User deleted.");
+				return;
+			}
+		}
+		System.out.println("User does not exist!");
 	}
 }
--- src/Backend/MainServer.java
+++ src/Backend/MainServer.java
@@ -1,5 +1,25 @@
 package Backend;
 
+import java.util.ArrayList;
+
+import Backend.Groups.*;
+
 public class MainServer {
+	//temp arraylists to store the users for testing
+	private ArrayList<User> DBUser;
+	private ArrayList<Admin> ABAdmin;
+	
+	public int AAA(int ID, String pass) {
+		//check the DB for the correct pass for the id
+		//ak toto citas simon tak skus zavolat tuto funkciu pri logine
+		/*
+		 0 - neuspesne prihlasenie, zle meno alebo heslo
+		 1 - uspesne prihlasenie
+		 2 - chybajuce meno
+		 3 - chybajuce heslo
+		 4 - neregistrovany uzivatel, prosim zaregistruj sa
+		*/
+		return 1;
+	}
 	
 }
